5c48b18ba9d4add7dba24d28eeb97764168c5ed7
Doreen162 <doreenligman17@gmail.com>
2020-09-23 14:42:22 +0200

    fix

---
+++ src/components copy/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div>
+      <h4>
+        Doreen Rowena Ligman
+        <i class='fa fa-copyright' aria-hidden='true'></i>2020
+      </h4>
+    </div>
+  );
+}
+
+export default Footer;
